c5323a69ef297597439ea1ac8c831c03613b1c9d
samsyntx <samaroraytb@gmail.com>
2024-09-30 03:26:42 +0530

    Settings DB and vercel  project.settings changes

--- server/project/settings.py
+++ server/project/settings.py
@@ -76,17 +76,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-DATABASES = {
-        'default': {
-            'ENGINE': os.getenv('DB_ENGINE'),
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
-        }
-    }
+DATABASES = {}
 
 DATABASES['default'] = dj_database_url.config()
 
